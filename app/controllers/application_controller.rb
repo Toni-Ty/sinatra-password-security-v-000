d63da0a52614b07054d5e062379f2e507a88ce8b
Toni-Ty <antonique2479@yahoo.com>
2019-10-13 05:46:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,13 +18,7 @@ class ApplicationController < Sinatra::Base
 
 	post "/signup" do
 		#your code here!
-  	user = User.new(:username => params[:username], :password => params[:password])
-			if user.save
-				redirect "/login"
-			else
-				redirect "/failure"
 	end
-end
 
 	get "/login" do
 		erb :login
@@ -32,13 +26,8 @@ end
 
 	post "/login" do
 		#your code here!
-		user = User.find_by(:username => params[username])
-		if user
-			redirect "/success"
-		else
-			redirect "/failure"
+		u
 	end
-end
 
 	get "/success" do
 		if logged_in?
